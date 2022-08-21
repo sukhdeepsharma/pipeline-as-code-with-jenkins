e45bc37f1105448caa3f513ad727000f6b115618
Ubuntu <azureuser@dws-vm.llqkd0vzldnejd15albasx1zyg.jx.internal.cloudapp.net>
2022-08-21 23:27:35 +0000

    update jenkinsfile, test_main.py

--- chapter8/pipelines/movies-loader/test_main.py
+++ chapter8/pipelines/movies-loader/test_main.py
@@ -15,7 +15,7 @@ class TestJSONLoaderMethods(unittest.TestCase):
     def test_title(self):
         self.assertEqual(self.movies[0]['title'], 'The Shawshank Redemption')
   
-   def test_id(self):
+    def test_id(self):
         self.assertEqual(self.movies[0]['id'], 'tt0111161')
  
 if __name__ == '__main__':
